0e843cda2a6ad0f4d530c0d1a52dd13d18cd30ec
Josh Kuhn <josh@rethinkdb.com>
2014-09-25 19:41:54 -0700

    River can now be installed and listens

--- src/main/java/org/elasticsearch/river/rethinkdb/RethinkDBRiver.java
+++ src/main/java/org/elasticsearch/river/rethinkdb/RethinkDBRiver.java
@@ -19,34 +19,35 @@
 
 package org.elasticsearch.river.rethinkdb;
 
-//import org.rethinkdb.RethinkDB;
-//import org.rethinkdb.RethinkDBConnection;
+import com.rethinkdb.Cursor;
+import com.rethinkdb.RethinkDB;
+import com.rethinkdb.RethinkDBConnection;
+import com.rethinkdb.ast.query.RqlQuery;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.common.inject.Inject;
+import org.elasticsearch.common.util.concurrent.EsExecutors;
 import org.elasticsearch.river.AbstractRiverComponent;
 import org.elasticsearch.river.River;
 import org.elasticsearch.river.RiverName;
 import org.elasticsearch.river.RiverSettings;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+
+import java.util.*;
 
 /**
  *
  */
 public class RethinkDBRiver extends AbstractRiverComponent implements River {
 
+    private static final RethinkDB r = RethinkDB.r;
     private final Client client;
 
-    //private final RethinkDBConnection reconn;
     public final String hostname;
     public final int port;
-    public final List<String> dbs;
+    public final String authKey;
+    private final ChangeInfos changeinfos;
 
-    private volatile boolean closed = false;
-
-    private volatile Thread thread;
+    private volatile List<Thread> threads;
+    private volatile boolean closed;
 
     @SuppressWarnings({"unchecked"})
     @Inject
@@ -57,33 +58,115 @@ public class RethinkDBRiver extends AbstractRiverComponent implements River {
         super(riverName, settings);
         this.client = client;
 
+        // Get ye settings from the document:
         Map<String, Object> rdbSettings = jsonGet(
                 settings.settings(), "rethinkdb", new HashMap<String, Object>());
 
         hostname = jsonGet(rdbSettings, "host", "localhost");
         port = jsonGet(rdbSettings, "port", 28015);
-        dbs = jsonGet(rdbSettings, "databases", new ArrayList<String>());
+        authKey = jsonGet(rdbSettings, "auth_key", "");
+        Map<String,? extends List<String>> tables = jsonGet(rdbSettings, "databases", new HashMap());
+
+        // Create thy changeinfos to hold the configuration
+        changeinfos = new ChangeInfos(tables);
+        threads = new ArrayList();
+        closed = false;
     }
 
     @Override
     public void start() {
+        // Start up all the listening threads
+        logger.info("Starting up RethinkDB River for {}:{}", hostname, port);
+        for(ChangeInfo changeinfo : changeinfos.getAll()) {
+            Thread thread = EsExecutors
+                    .daemonThreadFactory(settings.globalSettings(), "rethinkdb_river")
+                    .newThread(new FeedWorker(changeinfo));
+            thread.start();
+            logger.info("Starting feed watcher for {}.{}", changeinfo.db, changeinfo.table);
+            threads.add(thread);
+        }
     }
 
     @Override
     public void close() {
+        if(closed){
+            return;
+        }
+        logger.info("Closing RethinkDB River");
+        closed = true;
+        for(Thread thread: threads){
+            thread.interrupt();
+        }
+
     }
 
-    private class Consumer implements Runnable {
+    private class FeedWorker implements Runnable {
+
+        private ChangeInfo changeinfo;
+        private RethinkDBConnection connection;
+        private Cursor<Map<String,Object>> cursor;
+
+        public FeedWorker(ChangeInfo changeinfo){
+            this.changeinfo = changeinfo;
+        }
         @Override
         public void run() {
+            connection = r.connect(hostname, port, authKey);
+            connection.use(changeinfo.db);
+            cursor = changeinfo.query.runForCursor(connection);
+            while(cursor.hasNext()){
+                Map<String,Object> change = cursor.next();
+                System.out.println(change); // TODO: actually insert this into ES
+            }
         }
     }
 
     @SuppressWarnings({"unchecked"})
-    public static <V> V jsonGet(
-            Map<String, Object> map,
-            String key,
-            V defaultValue){
-        return map.containsKey(key) ? ((Map<String, V>)map).get(key) : defaultValue;
+    public static <V> V jsonGet(Map<String,Object> map, String key, V defaultValue){
+        return map.containsKey(key) ? (V) map.get(key) : defaultValue;
+    }
+
+    public class ChangeInfo {
+        public final RqlQuery query;
+        public final String table;
+        public final String db;
+
+        private RethinkDBConnection connection;
+        private Cursor<Map<String,Object>> cursor;
+
+        public ChangeInfo(String db,String table){
+            this.db = db;
+            this.table = table;
+            query = r.table(table).changes();
+        }
+    }
+
+    public class ChangeInfos extends HashMap<String, HashMap<String, ChangeInfo>> {
+
+        public int totalSize;
+
+        public ChangeInfos(Map<String,? extends List<String>> dbs){
+            super();
+            totalSize = 0;
+            for(String db: dbs.keySet()){
+                for(String table: dbs.get(db)){
+                    this.getOrDefault(db, new HashMap<String, ChangeInfo>())
+                            .put(table, new ChangeInfo(db, table));
+                    totalSize++;
+                }
+            }
+        }
+
+        public ChangeInfo get(String db, String table){
+            return get(db).get(table);
+        }
+
+        public List<ChangeInfo> getAll(){
+            ArrayList<ChangeInfo> ret = new ArrayList<ChangeInfo>(totalSize);
+            for(HashMap tables: values()){
+                ret.addAll(tables.values());
+            }
+            return ret;
+        }
     }
 }
